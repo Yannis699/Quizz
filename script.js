0de908d70035249f11bc63570954882d67319932
Yannis699 <yannis.cristini@gmail.com>
2022-03-09 22:22:27 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const questionContainer = document.querySelector(".click-event");
+const btn1 = document.querySelector("#btn-1");
+const btn2 = document.getElementById("btn-2");
+const response = document.querySelector("p");
+
+
+questionContainer.addEventListener("click", () => {
+  questionContainer.classList.toggle("question-clicked");
+});
+
+btn1.addEventListener('click', () => {
+    response.classList.add("show-response");
+    response.style.background = 'green';
+});
+
+btn2.addEventListener('click', () => {
+    response.classList.add('show-response');
+    response.style.background = "red";
+});
+
+const mousemove = document.querySelector('.mousemove');
+console.log(mousemove);
